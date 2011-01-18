37e5362b9c0646c2756e61946133f42cf8ae27e6
Olex <tholex@gmail.com>
2011-01-18 14:26:19 -0500

    Rails command results (rails new podloko -d mysql)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Podloko::Application.config.secret_token = '61666be7f3620c1b9c541e46f604a0474a1c794a9673b385e2152ff3e75eb089523d1a383d315ce1fba454f399e1e24cb942b7a35e6b82187ef3bdb7e1ceedbc'
