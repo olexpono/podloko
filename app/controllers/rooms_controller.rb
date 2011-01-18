a76dbfdf2b5e99f4a4f5a9ae923980cbc0bfd819
Olex <tholex@gmail.com>
2011-01-18 15:49:45 -0500

    Clean out extraneous html and prepare for iPhone dev/debug

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -9,13 +9,18 @@ class RoomsController < ApplicationController
   end
 
   ### ACCESSED VIA PHONE (API) ###
-
+  skip_before_filter :verify_authenticity_token, 
+                :only => [:ping, :update_library]
   # occasionally pingback from phone, should update recently_active
   def ping
+    render :text => {"success" => false}.to_json
   end
 
   # POST from the phone with ipod library (query result)
   def update_library
+    json = ActiveSupport::Base64.decode64 params[:library]
+    puts " ---- JSON FROM PHONE ----\n\n#{json}\n\n"
+    render :text => {"success" => false}.to_json
   end
 
 end
